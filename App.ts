6c6ca443c192b06762cdaf544ddc12592611eb3c
915_Onodi_Bianca <bianca.onodi@stud.ubbcluj.ro>
2024-04-05 22:48:28 +0300

    saving my progress

--- App.ts
+++ App.ts
@@ -1,23 +1,42 @@
-
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from 'cors';
+import Dogs from "./Dogs";
 dotenv.config();
 
 const app: Express = express();
-app.use(cors<Request>());
+app.use(cors());
+
 const port = process.env.PORT;
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("All good!");
+
+// gets list
+app.get("/dogs", (req: Request, res: Response) => {
+    res.send({Dogs});
 });
+
+//get one
+app.get("/dogs/:id", (req: Request, res: Response) => {
+    console.log(req.params.id)
+    const searchId = req.params.id;
+    const foundDog = Dogs.find(dog => dog.id === searchId)
+    if (!foundDog) {
+        res.send(undefined);
+        return
+    }
+    res.send(foundDog)
+});
+
+//post
+
+//patch/ update
+
+//delete
 
 app.listen(port, () => {
     console.group();
     console.log(`Server started at port ${port}`);
     console.groupEnd();
 });
-
-app.use(cors())
 
 export default app;
---
+++ Dogs.ts
@@ -0,0 +1,26 @@
+type DogType = {
+    id: string;
+    Name: string;
+    Age: number;
+    Breed: string;
+}
+let Dogs:DogType[]=[
+    {
+        id:"1",
+        Name:"Max",
+        Age:3,
+        Breed:"Dalmatian"
+    },{
+        id:"2",
+        Name:"Daisy",
+        Age:7,
+        Breed:"Daschund"
+    },{
+        id:"3",
+        Name:"Buck",
+        Age:1,
+        Breed:"Husky"
+    }
+]
+
+export default  Dogs;
